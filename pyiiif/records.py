e87b100140d7e742a72a03246c454609f62dcdc2
brian <balsamo@uchicago.edu>
2018-02-08 16:58:44 -0600

    Tossing this schema in the repo root for the moment. Also, boilerplate getters and setters on various record classes.

--- pyiiif/records.py
+++ pyiiif/records.py
@@ -28,6 +28,24 @@ class Record:
         """
         return json.dump(self.j, flo, **kwargs)
 
+    def get_type(self):
+        return self.j.get('@type')
+
+    def set_type(self, x):
+        self.j['@type'] = x
+
+    def del_type(self, x):
+        self.j['@type'] = None
+
+    def get_id(self):
+        return self.j.get('@id')
+
+    def set_id(self, x):
+        self.j['@id'] = x
+
+    def del_id(self):
+        del self.j['@id']
+
     @classmethod
     def load(cls, *args, **kwargs):
         """
@@ -72,41 +90,207 @@ class Record:
         """
         raise NotImplemented()
 
+    type = property(get_type, set_type, del_type)
+    id = property(get_id, set_id, del_id)
+
 
 class Collection(Record):
-    pass
+    def __init__(self):
+        pass
+
+    def get_collections(self):
+        pass
+
+    def set_collections(self):
+        pass
+
+    def del_collections(self):
+        pass
+
+    def get_manifests(self):
+        pass
+
+    def set_manifests(self):
+        pass
+
+    def del_manifests(self):
+        pass
+
+    def get_members(self):
+        pass
+
+    def set_members(self):
+        pass
+
+    def del_members(self):
+        pass
+
+    collections = property(get_collections, set_collections, del_collections)
+    manifests = property(get_manifests, set_manifests, del_manifests)
+    members = property(get_members, set_members, del_members)
 
 
 class Manifest(Record):
-    pass
+    def __init__(self):
+        pass
+
+    def get_sequences(self):
+        pass
+
+    def set_sequences(self):
+        pass
+
+    def del_sequences(self):
+        pass
+
+    def get_structures(self):
+        pass
+
+    def set_structures(self):
+        pass
+
+    def del_structures(self):
+        pass
+
+    sequences = property(get_sequences, set_sequences, del_sequences)
+    structures = property(get_structures, set_structures, del_structures)
 
 
 class Range(Record):
-    pass
+    def __init__(self):
+        pass
+
+    def get_members(self):
+        pass
+
+    def set_members(self):
+        pass
+
+    def del_members(self):
+        pass
+
+    def get_canvases(self):
+        pass
+
+    def set_canvases(self):
+        pass
+
+    def del_canvases(self):
+        pass
+
+    memebers = property(get_members, set_members, del_members)
+    canvases = property(get_canvases, set_canvases, del_canvases)
 
 
 class Layer(Record):
-    pass
+    def __init__(self):
+        pass
+
+    def get_otherContent(self):
+        pass
+
+    def set_otherContent(self):
+        pass
+
+    def del_otherContent(self):
+        pass
+
+    otherContent = property(get_otherContent, set_otherContent, del_otherContent)
 
 
 class AnnotationList(Record):
-    pass
+    def __init__(self):
+        pass
+
+    def get_resources(self):
+        pass
+
+    def set_resources(self):
+        pass
+
+    def del_resources(self):
+        pass
+
+    resources = property(get_resources, set_resources, del_resources)
 
 
 class Sequence(Record):
-    pass
+    def __init__(self):
+        pass
+
+    def get_canvases(self):
+        pass
+
+    def set_canvases(self):
+        pass
+
+    def del_canvases(self):
+        pass
+
+    canvases = property(get_canvases, set_canvases, del_canvases)
 
 
 class Canvas(Record):
-    pass
+    def __init__(self):
+        pass
+
+    def get_images(self):
+        pass
+
+    def set_images(self):
+        pass
+
+    def del_images(self):
+        pass
+
+    def get_otherContent(self):
+        pass
+
+    def set_otherContent(self):
+        pass
+
+    def del_otherContent(self):
+        pass
+
+    images = property(get_images, set_images, del_images)
+    otherContent = property(get_otherContent, set_otherContent, del_otherContent)
 
 
 class Annotation(Record):
-    pass
+    def __init__(self):
+        pass
 
 
-class Content(Record):
-    pass
+class ImageContent(Record):
+    def __init__(self):
+        pass
+
+    def get_format(self):
+        pass
+
+    def set_format(self):
+        pass
+
+    def del_format(self):
+        pass
+
+    format = property(get_format, set_format, del_format)
+
+
+class OtherContent(Record):
+    def __init__(self):
+        pass
+
+    def get_format(self):
+        pass
+
+    def set_format(self):
+        pass
+
+    def del_format(self):
+        pass
+
+    format = property(get_format, set_format, del_format)
 
 
 # JSON-LD @types to Class
@@ -119,5 +303,5 @@ ttc = {
     "sc:Sequence": Sequence,
     "sc:Canvas": Canvas,
     "oa:Annotation": Annotation,
-    "dctypes:Image": Content
+    "dctypes:Image": ImageContent
 }
