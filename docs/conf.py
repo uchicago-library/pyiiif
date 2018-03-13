5ced8f22feb3c8cc887f4ed1047ebc8bc3d3190a
Tyler Danstrom <tdanstrom@uchicago.edu>
2018-03-13 10:30:15 -0500

    changed author info in docs

--- docs/conf.py
+++ docs/conf.py
@@ -50,8 +50,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyiiif'
-copyright = '2018, Brian Balsamo'
-author = 'Brian Balsamo'
+copyright = '2018, Tyler Danstrom'
+author = 'Tyler Danstrom'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -146,7 +146,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'pyiiif.tex', 'pyiiif Documentation',
-     'Brian Balsamo', 'manual'),
+     'Tyler Danstrom', 'manual'),
 ]
 
 
