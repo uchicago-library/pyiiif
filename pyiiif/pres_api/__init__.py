114e72859dfecd1e93b88e90be31f7680a0374eb
verbalhanglider <tdanstrom@uchicago.edu>
2018-04-05 13:28:28 -0500

    cleanup work
    - removed empty threedotzero package
    - added TODO to watch IIIF Presentation 3.0 spec and add 3.0 classes
      when that becomes stable

---
+++ pyiiif/pres_api/__init__.py
@@ -0,0 +1,3 @@
+# TODO: watch http://prezi3.iiif.io/api/presentation/3.0/#status-of-this-document
+# need to add classes to buid Presentation 3.0 records when IIIF 
+# Presentation 3.0 becomes newest version
